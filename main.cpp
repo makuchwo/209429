ed0b83c48cb2a7d047ddad9ceb27244651368a19
makuchwo <209429@student.pwr.wroc.pl>
2015-05-21 11:48:26 +0000

    lab

--- main.cpp
+++ main.cpp
@@ -1,56 +1,78 @@
 #include <iostream>
-#include <windows.h>
-#include <fstream>
-
-#include "generuj.hh"
-
 
 using namespace std;
 
-/*!
-\mainpage Program sluzacy do pomiaru zlozonosci obliczeniowej.
-\author Wojcich Makuch
-\date 10.03.2015
-\version 1.0
+class CNode {
+public:
+    int value;
+};
 
-Zadaniem programu jest wygenerowanie tablic
-liczb pseldoloswych oraz pomiar zlozonosci obliczeniowej polegajacej
-na wymnozeniu kazdego z tych elementow przez 2.
-Program zapisuje dane w pliku o nazwie Pomiar_czasu1.txt.
+class CEdge {
+public:
+    CNode *prev;
+    CNode *next;
+};
 
-\section wartosci
-Program wykonuje obliczenia dla tablic o rozmiarach:
-10-10000000
-*/
+class CGraph {
+public:
+    int V,E;
+    CNode *ListV;
+    CEdge *ListE;
+
+public:
+    CGraph(int v, int e);
+    void set_graph(int edge1, int vertice1, int vertice2);
+    CEdge* search(int key);
+};
+
+   CGraph::CGraph(int v, int e) : V(v), E(e)
+    {
+        ListV = new CNode[V];
+        ListE = new CEdge[E];
+    }
+
+    void CGraph::set_graph(int edge1, int verticle1, int verticle2)
+    {
+        ListV[verticle1].value=verticle1;
+        ListV[verticle2].value=verticle2;
+        ListE[edge1].prev=&ListV[verticle1];
+        ListE[edge1].next=&ListV[verticle2];
+    }
+
+    CEdge* CGraph::search(int key)
+    {
+        CEdge *tmp = new CEdge();
+
+        for(int i=0; i<E; i++)
+        {
+            if(key!=ListE[i].prev->value && key!=ListE[i].next->value)
+            {
+                tmp[i]=ListE[i];
+                cout << tmp[i].next->value << endl;
+                cout << tmp[i].prev->value << endl;
+            }
+            cout << endl;
+        }
+        return tmp;
+    }
 
 int main()
 {
-    int rozm=100000000;
-    int czas=0;
-       fstream plik;
+  CGraph *G1 = new CGraph(9,11);
+    G1->set_graph(0,0,1);
+    G1->set_graph(1,0,2);
+    G1->set_graph(2,1,3);
+    G1->set_graph(3,1,4);
+    G1->set_graph(4,3,6);
+    G1->set_graph(5,4,6);
+    G1->set_graph(6,2,4);
+    G1->set_graph(7,2,6);
+    G1->set_graph(8,2,5);
+    G1->set_graph(9,5,7);
+    G1->set_graph(10,7,8);
+    G1->set_graph(11,3,3); // zapomnialem o tym, niech bedzie to krawedz 11...
 
-    plik.open("Pomiar_czasu1.txt",ios::out);
-    if(!plik.good())
-    {
-        cerr << "Blad otwarcia pliku do zapisu." << endl;
-        return -1;
-    }
-
-int i,j;
-for(i=10,j=50; i<=rozm, j<=rozm; i*=10,j*=10)
-{
-    dane *d=new dane(i);
-    dane *d2=new dane(j);
-    d->generuj();
-    d2->generuj();
-    czas=d->licz();
-    cout << i << "\t\t " << czas << endl;
-    plik << i << "\t\t " << czas << endl;
-    czas=d2->licz();
-    cout << j << "\t\t " << czas << endl;
-    plik << j << "\t\t " << czas << endl;
+// SEARCH WYSWIETLA KRAWEDZIE PRZEZ KTORE PRZESZEDL
+    G1->search(8);
+    return 0;
 }
-    plik.close();
-    system("PAUSE");
-}
-
